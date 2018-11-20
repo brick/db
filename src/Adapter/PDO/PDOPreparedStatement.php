13c99f734b0577fabec090847d583834e936c8a6
Benjamin Morel <benjamin.morel@gmail.com>
2018-11-20 23:39:47 +0100

    Merge PreparedStatement::bind() and execute()

--- src/Adapter/PDO/PDOPreparedStatement.php
+++ src/Adapter/PDO/PDOPreparedStatement.php
@@ -12,21 +12,41 @@ class PDOPreparedStatement extends PDOStatement implements PreparedStatement
     /**
      * @inheritdoc
      */
-    public function bind($param, $value, int $type) : void
+    public function execute(array $parameters = []) : void
     {
-        // @todo check if this can throw an exception
-        $result = @ $this->pdoStatement->bindValue($param, $value, $type);
+        foreach ($parameters as $key => $parameter) {
+            if ($parameter === null) {
+                $type = \PDO::PARAM_NULL;
+            } elseif (is_string($parameter)) {
+                $type = \PDO::PARAM_STR;
+            } elseif (is_int($parameter)) {
+                $type = \PDO::PARAM_INT;
+            } elseif (is_float($parameter)) {
+                $type = \PDO::PARAM_STR;
+                $parameter = (string) $parameter;
+            } elseif (is_bool($parameter)) {
+                $type = \PDO::PARAM_BOOL;
+            } elseif (is_resource($parameter)) {
+                $type = \PDO::PARAM_LOB;
+            } else {
+                throw new DbException('Unsupported parameter type: ' . gettype($parameter));
+            }
 
-        if ($result === false) {
-            throw new DbException(); // @todo
+            if (is_int($key)) {
+                $key++;
+            }
+
+            try {
+                $result = $this->pdoStatement->bindValue($key, $parameter, $type);
+            } catch (\PDOException $e) {
+                throw new DbException(); // @todo
+            }
+
+            if ($result === false) {
+                throw new DbException(); // @todo
+            }
         }
-    }
 
-    /**
-     * @inheritdoc
-     */
-    public function execute() : void
-    {
         try {
             $result = @ $this->pdoStatement->execute();
         } catch (\PDOException $e) {
--- src/PreparedStatement.php
+++ src/PreparedStatement.php
@@ -7,24 +7,31 @@ namespace Brick\Db;
 interface PreparedStatement extends Statement
 {
     /**
-     * Binds a value to a parameter.
-     *
-     * @param string|int $param
-     * @param mixed      $value
-     * @param int        $type
-     *
-     * @return void
-     *
-     * @throws DbException
-     */
-    public function bind($param, $value, int $type) : void;
-
-    /**
      * Executes this prepared statement.
      *
+     * The parameters array must contain exactly the number of bound parameters in the SQL statement being executed,
+     * otherwise an exception is thrown.
+     *
+     * The array can be an associative array for prepared statements using named parameters, or a 0-based numeric array
+     * for prepared statements using question mark placeholders. Note that not all drivers support named parameters.
+     *
+     * The type of each parameter must be one of:
+     *
+     * - null
+     * - string
+     * - int
+     * - float
+     * - bool
+     * - resource
+     *
+     * If a resource is given, it is read from the current position until EOF.
+     * Other types (array, object) will throw an exception.
+     *
+     * @param array $parameters An array of values with as many elements as there are bound parameters.
+     *
      * @return void
      *
      * @throws DbException
      */
-    public function execute() : void;
+    public function execute(array $parameters = []) : void;
 }
