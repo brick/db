4013461ada77027e79a7abd47a10c8f2d1b7723d
Benjamin Morel <benjamin.morel@gmail.com>
2019-12-21 17:11:10 +0100

    CS fix

--- src/Driver/DriverException.php
+++ src/Driver/DriverException.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Brick\Db\Driver;
 
+use Throwable;
+
 class DriverException extends \Exception
 {
     /**
@@ -23,10 +25,10 @@ class DriverException extends \Exception
     /**
      * DbException constructor.
      *
-     * @param string          $message      The error message.
-     * @param string|null     $sqlState     The five characters SQLSTATE error code, or null if not available.
-     * @param string|int|null $errorCode    The driver-specific error code, or null if not available.
-     * @param \Throwable|null $previous     The previous exception, if any.
+     * @param string          $message   The error message.
+     * @param string|null     $sqlState  The five characters SQLSTATE error code, or null if not available.
+     * @param string|int|null $errorCode The driver-specific error code, or null if not available.
+     * @param Throwable|null  $previous  The previous exception, if any.
      */
     public function __construct(string $message, ?string $sqlState = null, $errorCode = null, ?\Throwable $previous = null)
     {
